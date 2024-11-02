feeee6bc7f0f7413606a7c655369a77be178479a
Miquel <miquel.work@gmail.com>
2024-11-02 11:21:07 +0000

    Failed attempt to add browser launch config / afterEach Custom Command to prevent Password Breach alert from being thrown after login

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,20 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome" && !browser.isHeadless) {
+          //Neither this or a custom command to handle and close alerts worked to prevent the password breach pop-up
+          //It would seem it is not a regular js alert bur rather a native Chrome notification
+          //This alert can be avoided by running headless mode, or incognito mode. Although incognito mode prevents the service worker fix mentioned in the README.
+          launchOptions.args.push("--disable-password-breach-detection");
+          launchOptions.args.push("--disable-features=PasswordManager");
+          launchOptions.preferences = {
+            "credentials_enable_service": false,
+            "profile.password_manager_enabled": false
+          };
+        }
+        return launchOptions;
+      });
     },
 
     baseUrl : 'https://www.saucedemo.com',
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,6 +18,3 @@ import '@applitools/eyes-cypress/commands'
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
